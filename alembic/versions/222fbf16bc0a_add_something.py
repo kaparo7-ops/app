55ee468526bb69290a38aa5d5e20817e3ecc0006
Mohammed Alameen <kaparo7@gmail.com>
2025-10-05 15:03:39 +0200

    Fix Alembic template; add migration 222fbf16bc0a

---
+++ alembic/versions/222fbf16bc0a_add_something.py
@@ -0,0 +1,22 @@
+"""add something
+
+Revision ID: 222fbf16bc0a
+Revises: 20250913_tender_ai_intake
+Create Date: 2025-10-05 12:59:26.801648
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '222fbf16bc0a'
+down_revision = '20250913_tender_ai_intake'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
